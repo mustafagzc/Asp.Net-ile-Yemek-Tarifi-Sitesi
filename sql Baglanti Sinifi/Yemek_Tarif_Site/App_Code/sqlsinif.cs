00c8617140792001732cdda3c0e9312751a3d1d8
Mustafa Gözcü <gozcummustafa@gmail.com>
2022-02-19 13:46:40 +0300

    Created SQL connect Class

---
+++ sql Baglanti Sinifi/Yemek_Tarif_Site/App_Code/sqlsinif.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for sqlsinif
+/// </summary>
+public class sqlsinif
+{
+    public SqlConnection baglanti()
+    {
+        SqlConnection baglan = new SqlConnection(@"Data Source=MUSTAFAGOZCU\SQLEXPRESS;Initial Catalog=yemektarifi;Integrated Security=True");
+        return baglan;
+    }
+}
