606f9d9bdce030beafc29579d5a6fef3a41e6f4e
Mustafa Gözcü <gozcummustafa@gmail.com>
2022-03-06 16:26:45 +0300

    page of meal detail  added

---
+++ YemekDetaySayfası/Yemek_Tarif_Site/YemekDetay.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Kullanici.master" AutoEventWireup="true" CodeFile="YemekDetay.aspx.cs" Inherits="YemekDetay" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+</asp:Content>
+
---
+++ YemekDetaySayfası/Yemek_Tarif_Site/YemekDetay.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class YemekDetay : System.Web.UI.Page
+{
+    sqlsinif bgl = new sqlsinif();
+    string yemekid = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        yemekid = Request.QueryString["yemekid"];
+
+        SqlCommand komut = new SqlCommand("Select YemekAd From tbl_yemekler where yemekid=@p1", bgl.baglanti());
+        komut.Parameters.AddWithValue("@p1", yemekid);
+        SqlDataReader dr = komut.ExecuteReader();
+        while (dr.Read())
+        {
+            Label3.Text = dr[0].ToString();
+        }
+        bgl.baglanti().Close();
+    }
+}
